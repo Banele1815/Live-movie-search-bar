77c3d1c95cb357cb97d1e46bde7cccfb79bf729d
kamololadm-png <kamololadm@gmail.com>
2026-05-07 15:57:06 +0200

    i have added a debounce timer

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("App loaded");
+
+const MOVIES = [
+  { title: 'Despicable Me', year: 2010, genre: 'Animation · Comedy' },
+  { title: 'Iron Man', year: 2008, genre: 'Action · Superhero' },
+  { title: 'Black Panther', year: 2018, genre: 'Action · Superhero' },
+  { title: 'Boss Baby', year: 2017, genre: 'Animation · Comedy'},
+  { title: 'High School Musical', year: 2006, genre: 'Musical · Drama' },
+];
+
+return text.replace(new RegExp(escaped, 'gi'), match => `<mark>${match}</mark>`);
+
+debounceTimer = setTimeout(() => {
+  render(); 
+}, 300);
+
