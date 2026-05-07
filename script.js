95d85779e7512805a95e71d20445001642ef5036
MadalitsoB <147288815+MadalitsoB@users.noreply.github.com>
2026-05-07 15:35:21 +0200

    i crated a no results and rest button

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("App loaded");
+const searchInput = document.getElementById("searchInput");
+const movies = document.querySelectorAll("#movieList li");
+const noResultsMessage = document.getElementById("noResultsMessage");
+const resetBtn = document.getElementById("resetBtn");
+
+searchInput.addEventListener("input", function () {
+  const searchValue = searchInput.value.toLowerCase();
+  let hasResults = false;
+
+  movies.forEach(function (movie) {
+    const text = movie.textContent.toLowerCase();
+
+    if (text.includes(searchValue)) {
+      movie.style.display = "list-item";
+      hasResults = true;
+    } else {
+      movie.style.display = "none";
+    }
+  });
+
+  // Show "No movies found"
+  noResultsMessage.style.display = hasResults ? "none" : "block";
+});
+
+// Reset everything
+resetBtn.addEventListener("click", function () {
+  searchInput.value = "";
+
+  movies.forEach(function (movie) {
+    movie.style.display = "list-item";
+  });
+
+  noResultsMessage.style.display = "none";
+});
