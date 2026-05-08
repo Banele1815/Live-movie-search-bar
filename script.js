78dd90095be75273a427d7c7f7d0fa7057b5ac2e
Bea <masengobea86@gmail.com>
2026-05-08 13:33:05 +0200

    Filter Logic Component complete

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("App loaded");
+const searchinput = document.getElementById("searchinput");
+const movies = document.querySelectorAll("#movielist li");
+
+searchinput.addEventListener("input", function () {
+  const searchValue = searchinput.value.toLowerCase();
+
+  movies.forEach(function (movie) {
+    const title = movie.querySelector("span").textContent.toLowerCase();
+
+    if (searchValue !== "" && title.includes(searchValue)) {
+      movie.style.display = "";
+    } else {
+      movie.style.display = "none";
+    }
+  });
+});
